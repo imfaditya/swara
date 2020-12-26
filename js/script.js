b8eb502f00c0051690114943a77df512ce5efda2
imfaditya <imamaditya2211@gmail.com>
2020-12-26 15:33:57 +0700

    Revisi konsep

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-const menuToggleInput = document.querySelector('.mainResponsiveCheckbox');
-const text = document.querySelector('.mainResponsiveTwo');
-const text2 = document.querySelector('.mainResponsiveThree');
-const text3 = document.querySelector('.mainResponsive');
-const text4 = document.querySelector('.mainResponsiveFour');
-const button = document.querySelector('.mainResponsiveBtnTwo')
-const button2 = document.querySelector('.mainResponsiveBtn');
-
-menuToggleInput.addEventListener('click', function(){
-    text.classList.toggle('clicked');
-    text2.classList.toggle('clicked');
-    text3.classList.toggle('clicked');
-    text4.classList.toggle('clicked');
-    button.classList.toggle('clicked');
-    button2.classList.toggle('clicked');
-})
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled', $(this).scrollTop()>50);
+  })
