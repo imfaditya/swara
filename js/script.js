8832ff18613922634de1a58ce83221669aad0690
imfaditya <imamaditya2211@gmail.com>
2021-01-06 21:05:10 +0700

    revisi js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-$(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop()>50);
-  })
-
   AOS.init();
+
+  var nav = document.getElementsByClassName('nav');
+  var nava = document.getElementsByClassName('nav a');
+
+
+  $(window).scroll(function () {
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 600);
+    $('nav a').toggleClass('scrolled', $(this).scrollTop() > 600);
+  })
